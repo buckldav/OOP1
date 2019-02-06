f378e8e186e76069e9aa06a5e170f42df2e8b113
buckldav <runalldayandnight@gmail.com>
2019-02-06 09:31:09 -0700

    Add files

---
+++ OOP1/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Person
+    {
+        // Constructor that takes no arguments:
+        public Person()
+        {
+            Name = "unknown";
+            FavoriteMovie = "unknown";
+        }
+
+        // Constructor that takes one argument:
+        public Person(string name)
+        {
+            Name = name;
+            FavoriteMovie = "unknown";
+        }
+
+        // Properties
+        public string Name { get; }
+        public string FavoriteMovie { get; set; }
+
+        // Method that overrides the base class (System.Object) implementation.
+        public override string ToString()
+        {
+            return $"{Name}'s favorite movie is {FavoriteMovie}.";
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person me = new Person("David");
+            me.FavoriteMovie = "Bill and Ted";
+            Person friend = new Person("George");
+            Console.WriteLine(me);
+            Console.WriteLine(friend);
+            Console.ReadKey();
+        }
+    }
+}
